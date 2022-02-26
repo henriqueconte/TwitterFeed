811a673a154046f4188c3a3497ffcd7531f832cc
Renan Magagnin <renanmagagnin@gmail.com>
2022-02-25 22:27:44 -0300

    Add constructors to Packet

--- shared/headers/packet.hpp
+++ shared/headers/packet.hpp
@@ -1,6 +1,8 @@
 #ifndef _PACKET_
 #define _PACKET_
 
+#include <iostream>
+
 #define BUFFER_LENGTH 256
 
 class Packet {
@@ -9,7 +11,17 @@ class Packet {
         int seqn;          // Sequence number (packet ID)
         int length;        // Payload size (packet size)
         long int timestamp;       // Timestamp from when the packet was created
-        char* message[BUFFER_LENGTH];         // Packet message (like "bla bla bla")
+        char message[BUFFER_LENGTH];         // Packet message (like "bla bla bla")
+
+        Packet() {}
+
+        Packet(std::string msg) {
+            type = 1; // TODO: Update this when values of type are defined
+            bzero(&message, BUFFER_LENGTH);
+            strncpy(message, msg.c_str(), BUFFER_LENGTH);
+            timestamp = time(NULL);
+            length = strlen(message);
+        }
 };
 
 #endif
